25ee3648e3fbe00cd5f8e8c7f720a1a74616b8b3
Filip Pranklin <filip.pranklin@gmail.com>
2019-06-20 17:39:46 +0200

    Add searchbar

--- app/src/main/java/hr/codable/reporter/MainActivity.kt
+++ app/src/main/java/hr/codable/reporter/MainActivity.kt
@@ -3,6 +3,8 @@ import android.os.Bundle
 import android.support.design.widget.TabLayout
 import android.support.v4.view.ViewPager
 import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.widget.SearchView
 import hr.codable.reporter.adapter.ViewPagerAdapter
 import hr.codable.reporter.fragment.EverythingFragment
 import hr.codable.reporter.fragment.TopHeadlinesFragment
@@ -14,6 +16,7 @@ class MainActivity : AppCompatActivity() {
     private var tabLayout: TabLayout? = null
     private var viewPager: ViewPager? = null
     private var viewPagerAdapter: ViewPagerAdapter? = null
+    private var searchView: SearchView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,14 +26,34 @@ class MainActivity : AppCompatActivity() {
         viewPager = findViewById(R.id.viewpager_id)
         viewPagerAdapter = ViewPagerAdapter(supportFragmentManager)
 
-        viewPagerAdapter?.addFragment(TopHeadlinesFragment(), "Top Headlines")
-        viewPagerAdapter?.addFragment(EverythingFragment(), "Everything")
+        viewPagerAdapter?.addFragment(TopHeadlinesFragment(), getString(R.string.top_headlines))
+        viewPagerAdapter?.addFragment(EverythingFragment(), getString(R.string.everything))
 
         viewPager?.adapter = viewPagerAdapter
         tabLayout?.setupWithViewPager(viewPager)
 
 
 //        LoadTopHeadlinesTask().execute()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        menuInflater.inflate(R.menu.menu_main, menu)
+        val searchItem = menu?.findItem(R.id.search_bar_id)
+        searchView = searchItem?.actionView as SearchView?
+        searchView?.queryHint = getString(R.string.search_in_everything)
+        searchView?.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+
+            override fun onQueryTextChange(newText: String): Boolean {
+                return false
+            }
+
+            override fun onQueryTextSubmit(query: String): Boolean {
+                return false
+            }
+        })
+
+        return super.onCreateOptionsMenu(menu)
     }
 }
 
