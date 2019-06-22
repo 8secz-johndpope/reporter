7c989dade53cd7c5f3239f3c37020f4c24083772
Filip Pranklin <filip.pranklin@gmail.com>
2019-06-23 01:43:33 +0200

    Use hidden API key

--- app/src/main/java/hr/codable/reporter/rest/retrofit/ReporterService.kt
+++ app/src/main/java/hr/codable/reporter/rest/retrofit/ReporterService.kt
@@ -1,5 +1,6 @@
 package hr.codable.reporter.rest.retrofit
 
+import hr.codable.reporter.APIKey
 import hr.codable.reporter.entity.Response
 import retrofit.http.GET
 import retrofit.http.Headers
@@ -7,15 +8,15 @@ import retrofit.http.Query
 
 interface ReporterService {
 
-    @Headers("X-Api-Key: fa2b4e8e826d4f578e36848a1e43c2b7")
+    @Headers("X-Api-Key: " + APIKey.apiKey)
     @GET("/top-headlines")
     fun getTopHeadlines(@Query("language") language: String, @Query("page") page: Int): Response
 
-    @Headers("X-Api-Key: fa2b4e8e826d4f578e36848a1e43c2b7")
+    @Headers("X-Api-Key: " + APIKey.apiKey)
     @GET("/everything")
     fun getEverything(@Query("q") keyword: String): Response
 
-    @Headers("X-Api-Key: fa2b4e8e826d4f578e36848a1e43c2b7")
+    @Headers("X-Api-Key: " + APIKey.apiKey)
     @GET("/everything")
     fun getEverything(@Query("q") keyword: String, @Query("page") page: Int): Response
 
